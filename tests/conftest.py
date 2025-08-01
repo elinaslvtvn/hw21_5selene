d49de948b5a43a6285ec63a6ca4ef6f95a440bd5
elinard <elinard95@gmail.com>
2025-08-01 17:25:07 +0500

    дз 5

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,16 @@
 #тут хранятся фикстуры
 import pytest
 from selene import browser
-
+from selenium import webdriver
 
 @pytest.fixture(scope = 'function', autouse=True)
 def browser_open():
     browser.open('https://demoqa.com/automation-practice-form')
-    browser.config.timeout = 15
+    browser.config.window_height = 1080
+    browser.config.window_width = 1920
+    driver_options = webdriver.ChromeOptions()
+    driver_options.page_load_strategy = 'eager'
+    browser.config.driver_options = driver_options
+    browser.config.timeout = 1
     yield
     browser.quit()
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,5 @@
 import os
 from selene import browser , be , have
-
-
 def test_add_form_selene():
     browser.element('#firstName').type("Имя")
     browser.element('#lastName').type("Фамилия")
@@ -12,7 +10,7 @@ def test_add_form_selene():
     browser.element('.react-datepicker__month-select').click()
     browser.element('option[value="0"]').should(be.visible).click()
     browser.element('option[value="1995"]').should(be.visible).click()
-    browser.element('.react-datepicker__day react-datepicker__day--001 react-datepicker__day--weekend').click()
+    browser.element('.react-datepicker__day.react-datepicker__day--001.react-datepicker__day--weekend').click()
     browser.element('#subjectsInput').type("Maths").press_enter()
     browser.element('label[for ="hobbies-checkbox-1"]').click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('test_file.txt'))
@@ -20,4 +18,4 @@ def test_add_form_selene():
     browser.element('#state').click().element('#react-select-3-option-0').click()
     browser.element('#city').click().element('#react-select-4-option-0').click()
     browser.element('#submit').click()
-    browser.element('[#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
