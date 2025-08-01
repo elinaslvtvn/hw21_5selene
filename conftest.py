c6fd06d5afba242043332cbf7cac21ac98521b21
elinard <elinard95@gmail.com>
2025-08-01 15:43:12 +0500

    дз 5

---
+++ conftest.py
@@ -0,0 +1,11 @@
+#тут хранятся фикстуры
+import pytest
+from selene import browser
+
+
+@pytest.fixture(scope = 'function', autouse=True)
+def browser_open():
+    browser.open('https://demoqa.com/automation-practice-form')
+    browser.config.timeout = 10
+    yield
+    browser.quit()
