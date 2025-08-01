41dbd8ffc3592d282c41d2d7dc8df25e66d89e1e
elinard <elinard95@gmail.com>
2025-08-01 16:22:22 +0500

    дз 5

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,11 +3,11 @@ from selene import browser , be , have
 
 
 def test_add_form_selene():
-    browser.element('#firstName').type('Имя')
+    browser.element('#firstName').type("Имя")
     browser.element('#lastName').type("Фамилия")
     browser.element('#userEmail').type("test@gmail.com")
     browser.element('label[for="gender-radio-1"]').click()
-    browser.element('[id="userNumber"]').type("+79991234567")
+    browser.element('#userNumber').type("79991234567")
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').click()
     browser.element('option[value="0"]').should(be.visible).click()
@@ -16,9 +16,8 @@ def test_add_form_selene():
     browser.element('#subjectsInput').type("Maths").press_enter()
     browser.element('label[for ="hobbies-checkbox-1"]').click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('test_file.txt'))
-    browser.element('#currentAddress').type('Address')
+    browser.element('#currentAddress').type("Address")
     browser.element('#state').click().element('#react-select-3-option-0').click()
     browser.element('#city').click().element('#react-select-4-option-0').click()
     browser.element('#submit').click()
-    browser.element('[id = "example-modal-sizes-title-lg"]').should(have.text('Thanks for submitting the form'))
-
+    browser.element('[#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
