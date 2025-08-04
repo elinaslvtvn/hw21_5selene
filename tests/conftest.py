6ee3344433ea5d33f01c9712267cba45f57a3b0e
elinard <elinard95@gmail.com>
2025-08-04 17:05:03 +0500

    #добавила проверки введенных полей после "Submit"

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,12 +5,14 @@ from selenium import webdriver
 
 @pytest.fixture(scope = 'function', autouse=True)
 def browser_open():
+    # browser.driver.execute_script("$('#fixedban').remove()")
+    # browser.driver.execute_script("$('footer').remove()")
     browser.open('https://demoqa.com/automation-practice-form')
     browser.config.window_height = 1080
     browser.config.window_width = 1920
     driver_options = webdriver.ChromeOptions()
     driver_options.page_load_strategy = 'eager'
     browser.config.driver_options = driver_options
-    browser.config.timeout = 1
+    browser.config.timeout = 4
     yield
     browser.quit()
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,7 +5,7 @@ def test_add_form_selene():
     browser.element('#lastName').type("Фамилия")
     browser.element('#userEmail').type("test@gmail.com")
     browser.element('label[for="gender-radio-1"]').click()
-    browser.element('#userNumber').type("79991234567")
+    browser.element('#userNumber').type("7999123456")
     browser.element('#dateOfBirthInput').click()
     browser.element('.react-datepicker__month-select').click()
     browser.element('option[value="0"]').should(be.visible).click()
@@ -15,7 +15,23 @@ def test_add_form_selene():
     browser.element('label[for ="hobbies-checkbox-1"]').click()
     browser.element('#uploadPicture').send_keys(os.path.abspath('test_file.txt'))
     browser.element('#currentAddress').type("Address")
-    browser.element('#state').click().element('#react-select-3-option-0').click()
+
+    browser.element('#state input').type('NCR').press_enter()
+
+
+
+
     browser.element('#city').click().element('#react-select-4-option-0').click()
     browser.element('#submit').click()
+    #добавила проверки введенных полей после "Submit"
     browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
+    browser.element('.table').should(have.text('Имя'))
+    browser.element('.table').should(have.text('Фамилия'))
+    browser.element('.table').should(have.text('test@gmail.com'))
+    browser.element('.table').should(have.text('7999123456'))
+    browser.element('.table').should(have.text('01 January,1995'))
+    browser.element('.table').should(have.text('Maths'))
+    browser.element('.table').should(have.text('Sports'))
+    browser.element('.table').should(have.text('Address'))
+    browser.element('.table').should(have.text('NCR Delhi'))
+    browser.element('.table').should(have.text('test_file.txt'))
